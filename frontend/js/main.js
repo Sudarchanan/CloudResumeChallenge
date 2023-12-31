a3d37d0fff36f3b43cf3372d36865c3b72b838ad
sudarchanan <sudarchananms@gmail.com>
2023-12-31 16:18:21 +0530

    Frontend Completed

---
+++ frontend/js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+ $(window).on('load', function() { 
+	 /*Preloder*/
+	 $(".loader").fadeOut(); 
+	 $("#preloder").delay(400).fadeOut("slow");
+ 
+ });
+ 
+ window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+// const productionApiUrl = '';
+// const localApiUrl = '';
+
+// const getVisitCount = () => {
+//     let count = 30;
+//     fetch(productionApiUrl).then(response => {
+//         return response.json()
+//     }).then(response =>{
+//         console.log("Website called function API.");
+//         count =  response.count;
+//         document.getElementById("counter").innerText = count;
+//     }).catch(function(error){
+//         console.log(error);
+//     });
+//     return count;
+// }
+ 
+// html
+/* <p>and this page has been viewed <a id="counter"></a> times :) </p>  */
