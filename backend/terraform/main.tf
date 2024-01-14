5568ac43ccebfc3e7a5fcee5f0703ddf14cd6157
sudarchanan <sudarchananms@gmail.com>
2024-01-14 06:50:16 +0530

    Terraform Added

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -1,13 +1,52 @@
-terraform{
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "=3.0.0"
-        }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.85.0"
     }
-    
+  }
+
 }
+
 provider "azurerm" {
   features {}
 }
 
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "name-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "name"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "name"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                = "name"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {
+
+    application_stack {
+      python_version = "3.9"
+    }
+  }
+}
